116c3c7e0ff1cf34d11feefb4ab8c3309d06c92d
Adrian Moldovan <adim.moldovan@gmail.com>
2021-02-12 18:53:51 +0200

    Added video record options on browser context reset

--- projects/plugins/jetpack/tests/e2e/lib/setup-env.js
+++ projects/plugins/jetpack/tests/e2e/lib/setup-env.js
@@ -86,6 +86,10 @@ async function setUserAgent() {
 	// Reset context as a workaround to set a custom user agent
 	await jestPlaywright.resetContext( {
 		userAgent: e2eUserAgent,
+		recordVideo: {
+			dir: 'output/videos/',
+			size: { width: 800, height: 600 },
+		},
 	} );
 
 	userAgent = await page.evaluate( () => navigator.userAgent );
